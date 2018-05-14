52cfb542f7e6bc324509ccda835c1a4b7ad615dd
Алексей Лепёшкин <33727336+AlexeyLyapeshkin@users.noreply.github.com>
2018-05-15 00:32:25 +0300

    Course\notfinal

---
+++ progressBarForArch.pas
@@ -0,0 +1,27 @@
+unit progressBarForArch;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ComCtrls;
+
+type
+  TForm1 = class(TForm)
+    ProgressBar: TProgressBar;
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  HelpFile: string;
+
+implementation
+
+{$R *.dfm}
+
+end.
